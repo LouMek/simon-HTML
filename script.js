654dae2731cf1372595c7af42ad2a330f278699c
LouMek <lmekhelef@gmail.com>
2025-01-26 16:42:34 +0100

    feat: If the player selects the wrong colour, the game resets (only on the last element)

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var storePressedButtons = new Array();
 var randomColour = new Array();
-randomColour.push(redBtn.name);
+
 
 const greenBtn = document.getElementById("greenBtn");
 const redBtn = document.getElementById("redBtn");
 const blueBtn = document.getElementById("blueBtn");
-const yellowBtn = document.getElementById("yellowBtn")
+const yellowBtn = document.getElementById("yellowBtn");
 
+var debugText = document.getElementById("debugText");
+var playerText = document.getElementById("playerText");
+
+randomColour.push(redBtn.name);
 
 const tabBtn = [greenBtn, redBtn, blueBtn, yellowBtn];
 
@@ -16,8 +20,15 @@ for (let btn of tabBtn) {
         console.table(randomColour);
         console.table(storePressedButtons);
 
+        isCorrect()
         addRandomColour()
         console.table(randomColour);
+
+
+
+        debugText.innerHTML = displayRandomColour();
+
+        playerText.innerHTML = displayPlayerColour();
 
     });
 }
@@ -27,7 +38,31 @@ function addRandomColour() {
     let colour = ["green", "red", "blue", "yellow"];
     let number = Math.floor(Math.random() * 4);
     randomColour.push(colour[number]);
-
 }
 
+//Debug fonction
+function displayRandomColour() {
+    let textRandomColour = "";
+    randomColour.forEach((colour) => {
+        textRandomColour = textRandomColour + colour + " ";
+    });
+    return textRandomColour;
+}
 
+//Debug fonction
+function displayPlayerColour() {
+    let textPlayerColour = "";
+    storePressedButtons.forEach((colour) => {
+        textPlayerColour = textPlayerColour + colour + " ";
+    });
+    return textPlayerColour;
+}
+
+function isCorrect() {
+
+    if (storePressedButtons[storePressedButtons.length - 1] != randomColour[randomColour.length - 1]) {
+        storePressedButtons.splice(0, storePressedButtons.length);
+        randomColour = ["red"];
+    }
+
+}
